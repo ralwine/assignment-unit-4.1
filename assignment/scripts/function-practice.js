4c21cba62c576f79e8f436b06ffe606832f7e93e
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-06-26 15:42:14 -0500

    getting started ... fix that 2nd exercise

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,11 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName(name) {
-  return;
+function helloName(Biscuit) {
+  return 'Hello, Biscuit!';
 }
 // Remember to call the function to test
-
+console.log('Hello,' + ' ' + helloName);
 
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber) {
